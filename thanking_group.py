1061dd62b1532140c5880a5f073a84ce5b502601
Novel-Y <novelghebre@gmail.com>
2025-01-06 18:23:59 +0300

    thanking code 1st edit

---
+++ thanking_group.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A a code that thanks every member of group-23
+
+Parameters:
+    - group_23: Takes an array of string input of group members. 
+
+@author: Novel Yonas
+
+Idea enriched from the book 'PYTHON CRUSH COURSE' by Eric Matthes.
+
+"""
+group_23 = ['Bhang', 'Ava', 'Ibrahim'] 
+for every in group_23: 
+    print(every.title() + ", that was a great time collabrating with you!")  
+    print("I can't wait to communicate with you in the future, " + every.title() + ".\n")
