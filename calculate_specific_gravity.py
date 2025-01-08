a02e7fb512d8a771dafc75d761ff7122835655dc
Novel-Y <novelghebre@gmail.com>
2025-01-08 04:18:14 +0300

    edited the return value

--- calculate_specific_gravity.py
+++ calculate_specific_gravity.py
@@ -2,30 +2,32 @@
 # -*- coding: utf-8 -*-
 def calculate_specific_gravity(weight_in_air, weight_in_water):
     """
-    The function calculates the specific gravity of a rock mass.
+      The function calculates the specific gravity of a rock mass.
 
-  Args:
-    weight_in_air: Takes float weight of the rock mass in air.
-    weight_in_water: Takes float weight of the rock mass submerged in water.
+    Args:
+      weight_in_air: Takes float weight of the rock mass in air.
+      weight_in_water: Takes float weight of the rock mass submerged in water.
 
-  Returns:
-    The calculated float specific gravity of the rock mass.
-  Raises:
-    'Error: when the denominator is zero, rock weight in air can not be same when 
-        weighed in water. 
-  >>> calculate_specific_gravity(2, 2)
-      "Error: recheck your measurements."
-  >>> calculate_specific_gravity(4, 2)
-      specific gravity:2.0
-  >>> calculate_specific_gravity(5, 2)  
-      specific gravity: 1.6666666666666667
+    Returns:
+      The calculated float specific gravity of the rock mass.
+    Raises:
+      'Error: when the denominator is zero.
+    >>> calculate_specific_gravity(2, 2)
+        "Error: recheck your measurements."
+    >>> calculate_specific_gravity(4, 2)
+        specific gravity:2.0
+    >>> calculate_specific_gravity(5, 2)
+        specific gravity: 1.6666666666666667
     """
-    if weight_in_air <= weight_in_water:        #checks the equality of the inputs:
-      #throws error: rock mass weighed in open air should be > in weighed in water:
+    if weight_in_air <= weight_in_water:  # checks the equality of the inputs:
+        # throws error: rock mass weighed in open air should be > in weighed in water:
         return "Error: recheck your measurements."
-      #if not equal->the specific gravity is calculated below:
-    specific_gravity = weight_in_air / (weight_in_air - weight_in_water) 
-    return f"specific gravity: {specific_gravity}"   #keeps the value of specific gravity
+    # if not equal->the specific gravity is calculated below:
+    specific_gravity = weight_in_air / (weight_in_air - weight_in_water)
+    return (
+        f"specific gravity: {specific_gravity}"  # keeps the value of specific gravity
+    )
+
 
 # Takes a weight measurements into float to comply with scientific notation:
 weight_air = float(input("Enter weight of the rock mass in air: "))
--- test_calculate_specific_gravity.py
+++ test_calculate_specific_gravity.py
@@ -15,22 +15,22 @@ class TestSpecificGravityCalculator(unittest.TestCase):
         """
         Test must pop 'Error: Weight in air and weight in water cannot be the same.'.
         """
-        actual = calculate_specific_gravity(2,2)
-        expected = 'Error: recheck your measurements.'
+        actual = calculate_specific_gravity(2, 2)
+        expected = "Error: recheck your measurements."
         self.assertTrue(actual == expected)
 
     def test_unequal_even_measurements_of_weight(self):
         """
         Test is being checked for unequal even measurements of weight.
         """
-        actual = calculate_specific_gravity(4,2)
+        actual = calculate_specific_gravity(4, 2)
         expected = 2.0
         self.assertTrue(actual == expected)
-        
+
     def test_unequal_odd_even_measurements_of_weight(self):
         """
         Test is being checked for unequal odd and even measurements of weight.
         """
-        actual = calculate_specific_gravity(5,2)
+        actual = calculate_specific_gravity(5, 2)
         expected = 1.6666666666666667
-        self.assertTrue(actual == expected)   
+        self.assertTrue(actual == expected)
