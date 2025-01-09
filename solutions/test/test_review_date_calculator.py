2dc9fd1ecfb4966900625523ec3ff3a2732cd97e
Novel-Y <novelghebre@gmail.com>
2025-01-09 16:31:28 +0300

    edited 2

---
+++ solutions/test/test_review_date_calculator.py
@@ -0,0 +1,25 @@
+import unittest
+from spaced_repetition import calculate_review_days
+
+
+class TestCalculateReviewDays(unittest.TestCase):
+
+    def test_start_day_1(self):
+        self.assertEqual(calculate_review_days(1), [2, 4, 8, 16])
+
+    def test_start_day_15(self):
+        self.assertEqual(calculate_review_days(15), [16, 18, 22, 30])
+
+    def test_start_day_30(self):
+        self.assertEqual(calculate_review_days(30), [31, 2, 6, 14])
+
+    def test_start_day_31(self):
+        self.assertEqual(calculate_review_days(31), [1, 3, 7, 15])
+
+    def test_start_day_edge(self):
+        # Testing an edge case not covered explicitly in docstring examples
+        self.assertEqual(calculate_review_days(28), [29, 31, 3, 11])
+
+
+if __name__ == "__main__":
+    unittest.main()
